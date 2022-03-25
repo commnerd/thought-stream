da868b131f0ac2fa650d5d4353204f25d70be7e4
Michael J. Miller <commnerd@gmail.com>
2022-03-25 12:19:34 -0600

    Added authentication to web interface

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::middleware('auth')->get('/', function () {
     return view('welcome');
 });
 
 Route::resource('thoughts', \App\Http\Controllers\Web\ThoughtController::class);
+
+Route::get('/login', [App\Http\Controllers\Web\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Web\Auth\LoginController::class, 'login']);
+
+Route::post('/logout', [App\Http\Controllers\Web\Auth\LoginController::class, 'logout'])->name('logout');
+
+Route::get('/password/confirm', [App\Http\Controllers\Web\Auth\ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+Route::post('/password/confirm', [App\Http\Controllers\Web\Auth\ConfirmPasswordController::class, 'confirm']);
+
+Route::post('/password/email', [App\Http\Controllers\Web\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('/password/reset', [App\Http\Controllers\Web\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/password/reset', [App\Http\Controllers\Web\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+Route::get('/password/reset/{token}', [App\Http\Controllers\Web\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+
+Route::post('/register', [App\Http\Controllers\Web\Auth\RegisterController::class, 'register']);
+Route::get('/register', [App\Http\Controllers\Web\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
